a729cec7c8260ffebe2496625556f5941ff11170
akk55636 <akk55636@edu.redu.fi>
2025-03-24 17:10:22 +0200

    e

--- netlify/functions/fetchRobloxData.js
+++ netlify/functions/fetchRobloxData.js
@@ -1,10 +1,18 @@
 exports.handler = async(event, context) => {
     const key = process.env.RobloxKey;
     const api = process.env.apiKey;
+    const secret = process.env.secretKey;
 
-    const { placeId } = event.queryStringParameters;
+    const { placeId, secretKey } = event.queryStringParameters;
 
     let link = `https://develop.roblox.com/v2/places/${placeId}`;
+
+    if (secretKey != secret) {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({ result: "Invalid Key"})
+        }
+    }
 
     try { 
         const response = await fetch(link, {
